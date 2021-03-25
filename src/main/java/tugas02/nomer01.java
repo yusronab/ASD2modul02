4b9de05fe3d79205b285f9ea278417346d296e5c
ASUS <ASUS@LAPTOP-K6L4EKTE>
2021-03-26 06:29:22 +0700

    second commit

--- src/main/java/tugas02/nomer01.java
+++ src/main/java/tugas02/nomer01.java
@@ -1,13 +1,16 @@
 package tugas02;
 public class nomer01 {
     public static void main(String[] args) {
-        int[][] matriks = new int [3][2];
+        int[][] matriks = new int [3][3];
         matriks[0][0] = 34;
         matriks[0][1] = 56;
+        matriks[0][2] = 41;
         matriks[1][0] = 45;
         matriks[1][1] = 36;
+        matriks[1][2] = 37;
         matriks[2][0] = 51;
         matriks[2][1] = 32;
+        matriks[2][2] = 46;
         for (int i=0; i<matriks.length; i++){
             for (int j=0; j<matriks[0].length; j++){
                 System.out.print(matriks[i][j]+" ");
